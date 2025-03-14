952a91a75d5cdec8a7dad230eb4a17cb9c3f77cd
andre-de <neubertandre@yahoo.de>
2025-03-14 10:32:30 +0100

    save

--- src/main.py
+++ src/main.py
@@ -17,6 +17,34 @@ if __name__ == "__main__":
     window.rowconfigure(0, minsize=600)
     window.rowconfigure(1, minsize=200)
 
+    #configure functions
+    def zoom_in():
+        print("Zoom in")
+        return
+    
+    def zoom_out():
+        print("Zoom out")
+        return
+    
+    def run_calculation():
+        print("Run calculation")
+
+    def go_up():
+        print("Go up")
+        return
+    
+    def go_down():
+        print("Go down")
+        return
+    
+    def go_left():
+        print("Go left")
+        return
+    
+    def go_right():
+        print("Go right")
+        return
+
 
 
     #configure and display first frame containing the canvas
@@ -68,15 +96,15 @@ if __name__ == "__main__":
 
     #todo: replace with arrow symbols
     #add button commands
-    btn0 = tk.Button(fr01, text="Zoom in")
-    btn5 = tk.Button(fr01, text="Zoom out")
+    btn0 = tk.Button(fr01, text="Zoom in", command=zoom_in)
+    btn5 = tk.Button(fr01, text="Zoom out", command=zoom_out)
 
     fr01_in = tk.Frame(fr01)
 
-    btn1 = tk.Button(fr01_in, text="^")
-    btn2 = tk.Button(fr01_in, text="<")
-    btn3 = tk.Button(fr01_in, text=">")
-    btn4 = tk.Button(fr01_in, text="v")
+    btn1 = tk.Button(fr01_in, text="^", command=go_up)
+    btn2 = tk.Button(fr01_in, text="<", command=go_left)
+    btn3 = tk.Button(fr01_in, text=">", command=go_right)
+    btn4 = tk.Button(fr01_in, text="v", command=go_down)
     btn1.grid(row = 0, column=1, sticky="n")
     btn2.grid(row=1, column=0, sticky="w")
     btn3.grid(row=1, column=2, sticky="e")
@@ -96,7 +124,7 @@ if __name__ == "__main__":
 
     #configure and display the fourth frame containing the start button and feedback
     fr11 = tk.Frame(fr_lower)
-    btn_start = tk.Button(fr11, text="Run!")
+    btn_start = tk.Button(fr11, text="Run!", command=run_calculation)
     lbl_space= tk.Label(fr11, text=" ")
     lbl_runtime = tk.Label(fr11, text="Runtime:")
     lbl_runtime_val = tk.Label(fr11, text="0 s")
@@ -110,6 +138,21 @@ if __name__ == "__main__":
     lbl_iterations_val.pack()
 
     fr11.grid(row=0, column=2, sticky="nsew")
+
+
+    #prepopulate entry elements and slider with values
+
+    def set_text(entry_widget, text):
+        entry_widget.delete(0, tk.END)
+        entry_widget.insert(0, text)
+        return
+    
+    set_text(ent1, "-2.0")
+    set_text(ent2, "1.0")
+    set_text(ent3, "-1.0")
+    set_text(ent4, "1.0")
+
+    sld0.set(1000)
 
 
 
