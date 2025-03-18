fe0c419824500a60e5d37fffb2a1f9dd44673fcb
andre-de <neubertandre@yahoo.de>
2025-03-18 15:07:46 +0100

    save

--- src/main.py
+++ src/main.py
@@ -17,46 +17,55 @@ if __name__ == "__main__":
     window.rowconfigure(0, minsize=600)
     window.rowconfigure(1, minsize=200)
 
-    #configure functions
-    def zoom_in():
-        print("Zoom in")
-        return
-    
-    def zoom_out():
-        print("Zoom out")
-        return
-    
-    def run_calculation():
-        print("Run calculation")
+    class Coordinates:
+        def __init__(self, x_min, x_max, y_min, y_max, width, height, frame):
+            self._x_min = x_min
+            self._x_max = x_max
+            self._y_min = y_min
+            self._y_max = y_max
+            self._width = width
+            self._height = height
+            self._frame = frame
+            self._canvas = Canvas(frame, width=WIDTH, height=HEIGHT, bg="#000000")
+            self._img = PhotoImage(width=WIDTH, height=HEIGHT)
 
-    def go_up():
-        print("Go up")
-        return
-    
-    def go_down():
-        print("Go down")
-        return
-    
-    def go_left():
-        print("Go left")
-        return
-    
-    def go_right():
-        print("Go right")
-        return
+        #configure functions
+        def zoom_in():
+            print("Zoom in")
+            return
+        
+        def zoom_out():
+            print("Zoom out")
+            return
+        
+        def run_calculation():
+            print("Run calculation")
 
-
+        def go_up():
+            print("Go up")
+            return
+        
+        def go_down():
+            print("Go down")
+            return
+        
+        def go_left():
+            print("Go left")
+            return
+        
+        def go_right():
+            print("Go right")
+            return
+        
+    
+    fr_upper = tk.Frame(window, borderwidth=5, relief=tk.GROOVE) #todo: make frame bigger than canvas
+    coord = Coordinates(-2.0, 1, -1.0, 1, WIDTH, HEIGHT, fr_upper)
 
     #configure and display first frame containing the canvas
-    fr_upper = tk.Frame(window, borderwidth=5, relief=tk.GROOVE) #todo: make frame bigger than canvas
-    canvas = Canvas(fr_upper, width=WIDTH, height=HEIGHT, bg="#000000")
-    canvas.pack()
-    img = PhotoImage(width=WIDTH, height=HEIGHT)
-    canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
+    coord._canvas.pack()
+    coord._canvas.create_image((WIDTH/2, HEIGHT/2), image=coord._img, state="normal")
     fr_upper.pack(fill=tk.BOTH)
-
-
     fr_lower = tk.Frame(window, borderwidth=5, relief=tk.GROOVE)
     fr_lower.pack(fill=tk.BOTH)
     fr_lower.columnconfigure(0, minsize=220)
@@ -96,15 +105,15 @@ if __name__ == "__main__":
 
     #todo: replace with arrow symbols
     #add button commands
-    btn0 = tk.Button(fr01, text="Zoom in", command=zoom_in)
-    btn5 = tk.Button(fr01, text="Zoom out", command=zoom_out)
+    btn0 = tk.Button(fr01, text="Zoom in", command=coord.zoom_in)
+    btn5 = tk.Button(fr01, text="Zoom out", command=coord.zoom_out)
 
     fr01_in = tk.Frame(fr01)
 
-    btn1 = tk.Button(fr01_in, text="^", command=go_up)
-    btn2 = tk.Button(fr01_in, text="<", command=go_left)
-    btn3 = tk.Button(fr01_in, text=">", command=go_right)
-    btn4 = tk.Button(fr01_in, text="v", command=go_down)
+    btn1 = tk.Button(fr01_in, text="^", command=coord.go_up)
+    btn2 = tk.Button(fr01_in, text="<", command=coord.go_left)
+    btn3 = tk.Button(fr01_in, text=">", command=coord.go_right)
+    btn4 = tk.Button(fr01_in, text="v", command=coord.go_down)
     btn1.grid(row = 0, column=1, sticky="n")
     btn2.grid(row=1, column=0, sticky="w")
     btn3.grid(row=1, column=2, sticky="e")
@@ -118,7 +127,7 @@ if __name__ == "__main__":
 
     #configure and display the fourth frame containing the start button and feedback
     fr11 = tk.Frame(fr_lower)
-    btn_start = tk.Button(fr11, text="Run!", command=run_calculation)
+    btn_start = tk.Button(fr11, text="Run!", command=coord.run_calculation)
     lbl_space= tk.Label(fr11, text=" ")
     lbl_runtime = tk.Label(fr11, text="Runtime:")
     lbl_runtime_val = tk.Label(fr11, text="0 s")
@@ -141,10 +150,10 @@ if __name__ == "__main__":
         entry_widget.insert(0, text)
         return
     
-    set_text(ent1, "-2.0")
-    set_text(ent2, "1.0")
-    set_text(ent3, "-1.0")
-    set_text(ent4, "1.0")
+    set_text(ent1, str(coord._x_min))
+    set_text(ent2, str(coord._x_max))
+    set_text(ent3, str(coord._y_min))
+    set_text(ent4, str(coord._y_max))
 
     sld0.set(1000)
 
